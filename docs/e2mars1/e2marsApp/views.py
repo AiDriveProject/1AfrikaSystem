c98f1a8404e9824ce1ccb798e3d06a560738106b
Daneil <danielbarakamhs@gmail.com>
2023-10-09 15:21:40 +0200

    Creating Account and log in

--- docs/e2mars1/e2marsApp/views.py
+++ docs/e2mars1/e2marsApp/views.py
@@ -1,10 +1,16 @@
+import json
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.urls import reverse
-from django.shortcuts import redirect
-from io import BytesIO
-import base64
-from django import template
+import subprocess
+from google.cloud import dialogflow
+from google.protobuf.json_format import MessageToJson
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+
 # Create your views here.
 
 def Home(required):
@@ -73,10 +79,10 @@ def LogIn(required):
         password = required.POST.get('password')
 
         if email:
-           # required.session['email'] = email
-           # required.session['password'] = password
+           required.session['email'] = email
+           required.session['password'] = password
            print(f'Email: {email} Password:{password} ')
-           # return redirect('DashBoard')
+           return HttpResponseRedirect('DashBoard')
 
     return render(required, "logins.html")
 
@@ -94,6 +100,8 @@ def Product(required):
             f'{pname} || {pprice} || {pdescription} || {pnumber} || {ppassword} || {pconpassword} || {gender}')
 
     return render(required, "ProductUpload.html")
+
+
 def Register(required):
 
 
@@ -105,9 +113,6 @@ def Register(required):
         phone = required.POST.get('phone')
         password = required.POST.get('password')
         conpassword = required.POST.get('conpassword')
-        import DatabaseConnection
-        DatabaseConnection.connection.reconnect()
-        DatabaseConnection.saving_in_MySQL_User_details_with_images("C:\\Users\\AppFactory\\Pictures\\Screenshots\\Images INsparation\\riana1.jpg", name, email, phone, service, password)
         print(
             f'Photo: {imageUpload} Email: {email} || Name: {name} || Sevice: {service} || Phone Number: {phone} || Password: {password} || Confirm Password: {conpassword}')
 
@@ -142,4 +147,52 @@ def DashBoard(required):
     if required.method == "POST":
         dashsearch = required.POST.get('dashsearch')
         print(f"Subscribe From Team Email : {dashsearch}")
-    return render(required, 'DashBoard.html', {'alldata': alldata, 'image': b64encode(data[1]), 'name': data[2]})
+    return render(required, "DashBoard.html")
+
+
+def convert(data):
+    if isinstance(data, bytes):
+        return data.decode('ascii')
+    if isinstance(data, dict):
+        return dict(map(convert, data.items()))
+    if isinstance(data, tuple):
+        return map(convert, data)
+
+    return data
+
+
+def detect_intent_text(project_id, session_id, text, language_code='en-US'):
+    project_root = os.path.dirname(os.path.abspath(__file__))
+    credentials_file_path = os.path.join(project_root, "AidriveCredentials.json")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_file_path
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def chatbot_view(request):
+    print('body', request.body)
+    imput_dict = convert(request.body)
+    if request.method == 'POST':
+        # user_input = request.POST.get('message', '')
+        user_input = json.loads(imput_dict)['message']
+        print(f"User Input: {user_input}")
+        if user_input.lower() == 'exit':
+            return JsonResponse({'botResponse': 'Goodbye!'})
+        response = detect_intent_text('aidrive-project', '0123D34', user_input)
+        return JsonResponse({'botResponse': response.fulfillment_text})
+        # return HttpResponse(response.fulfillment_text, status=200)
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'})
